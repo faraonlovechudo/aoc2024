dc67b539935c9e093ed46b61cdff8fe20d8e8569
faraonlovechudo <92992740+faraonlovechudo@users.noreply.github.com>
2024-12-03 23:43:25 +0100

    completed day 2, part 1

--- day2/02.c
+++ day2/02.c
@@ -6,31 +6,32 @@
 #define MAX_LEVELS 8
 
 int getReports(char filename[], char rawReports[][MAX_LENGHT]);
-void stringToInt(char rawReport[][MAX_LENGHT], int items, int report[][MAX_LEVELS]);
-void checkConditions(int reports[][MAX_LEVELS], int items);
+void stringToInt(char rawReport[][MAX_LENGHT], int items, int report[][MAX_LEVELS], int levels[]);
+void checkConditions(int report[][MAX_LEVELS], int items, int safe[], int levels[]);
+int sumItems(int a[], int entries);
 
 int main()
 {
     char rawReport[MAX_REPORTS][MAX_LENGHT];
     /*Since none of the levels equals 0, i will initialize all of the values to prevent random numbers*/
     int report[MAX_REPORTS][MAX_LEVELS] = {{0}};
-    int i, j;
+    int levels[MAX_REPORTS];
+    int safe[MAX_REPORTS] = {0};
     int items;
+    int total;
 
     /*New approach: get all reports as a whole, then separate the levels*/
     items = getReports("data.txt", rawReport);
-    stringToInt(rawReport, items, report);
+    stringToInt(rawReport, items, report, levels);
+    
+    checkConditions(report, items, safe, levels);    
 
-    for (i = 0; i < 1000; i++) {
-        for (j = 0; j < MAX_LEVELS; j++) {
-            printf("%d ", report[i][j]);
-        }
-        printf("\n");
-    }
+    total = sumItems(safe, items);
+
+    printf("Safe Reports: %d", total);
 
     return 0;
 }
-
 
 int getReports(char filename[], char rawReports[][MAX_LENGHT])
 {
@@ -46,7 +47,7 @@ int getReports(char filename[], char rawReports[][MAX_LENGHT])
     return i;
 }
 
-void stringToInt(char rawReport[][MAX_LENGHT], int items, int report[][MAX_LEVELS])
+void stringToInt(char rawReport[][MAX_LENGHT], int items, int report[][MAX_LEVELS], int levels[])
 {
     int i;
 
@@ -61,21 +62,55 @@ void stringToInt(char rawReport[][MAX_LENGHT], int items, int report[][MAX_LEVEL
             token = strtok(NULL, " ");
             j++;
         }     
+        
+        levels[i] = j;
     }
 }
 
 
-void checkConditions(int reports[][MAX_LEVELS], int items)
+void checkConditions(int report[][MAX_LEVELS], int items, int safe[], int levels[])
 {
-    int i, j = 0;
+    int i, j, a, count = 0;
 
     for (i = 0; i < items; i++)
     {
-        for (j = 0; j < MAX_LEVELS; j++)
+        count = 0;
+        /*if first level is greater than the second, then all must be and vice versa*/
+        if (report[i][0] < report[i][1])
         {
+            for (j = 1; j < levels[i]; j++)
+            {
+                    a = report[i][j] - report[i][j - 1];
+                    if ( 1 <= a && a <= 3)
+                        count++;               
+            }
             
-
+            if (levels[i] == (count + 1))
+                safe[i] = 1;
         }
 
+        if (report[i][0] > report[i][1])
+        {
+            for (j = 1; j < levels[i]; j++)
+            {
+                    a = report[i][j - 1] - report[i][j];
+                    if ( 1 <= a && a <= 3)
+                        count++;                
+            }
+            
+            if (levels[i] == (count + 1))
+                safe[i] = 1;            
+        }
     }
+}
+
+int sumItems(int a[], int entries)
+{
+    int i;
+    int sum = 0;
+
+    for (i = 0; i < entries; i++)
+        sum += a[i];
+
+    return sum;
 }
